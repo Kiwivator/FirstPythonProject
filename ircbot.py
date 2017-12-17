baccce30fc22924eb53e384d9c735243896be659
MotivatorNZ <34211218+MotivatorNZ@users.noreply.github.com>
2017-12-17 22:02:46 +0900

    Update ircbot.py

--- ircbot.py
+++ ircbot.py
@@ -23,7 +23,6 @@ if __name__ == '__main__':
 	ircsock.connect((server, 6667))
 	ircsock.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick + " " + botnick + "\n", "UTF-8"))
 	ircsock.send(bytes("NICK "+ botnick +"\n", "UTF-8"))
-	joinchan(channel)
 	
 	while True:
 		ircmsg = ircsock.recv(2048).decode("UTF-8")
@@ -33,6 +32,7 @@ if __name__ == '__main__':
 		msgcode = ircmsg.split()[0]
 
 		if ircmsg == "001":
+			joinchan(channel)
 			joinchan(channel)
 		
 		if msgcode == "PRIVMSG":
