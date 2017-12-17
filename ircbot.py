6932f1aa8660bbeb52748041c098d834322b1399
MotivatorNZ <34211218+MotivatorNZ@users.noreply.github.com>
2017-12-17 20:04:14 +0900

    Update ircbot.py

--- ircbot.py
+++ ircbot.py
@@ -23,6 +23,7 @@ if __name__ == '__main__':
 	ircsock.connect((server, 6667))
 	ircsock.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick + " " + botnick + "\n", "UTF-8"))
 	ircsock.send(bytes("NICK "+ botnick +"\n", "UTF-8"))
+	ircmsg = ""
 	if ircmsg.find("001") == -1:
 		ircmsg = ircsock.recv(2048).decode("UTF-8")
 		ircmsg = ircmsg.strip('\n\r')
