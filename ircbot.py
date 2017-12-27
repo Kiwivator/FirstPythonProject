47085a0644b43e983b7d025e5704033653993a82
MotivatorNZ <34211218+MotivatorNZ@users.noreply.github.com>
2017-12-27 18:32:19 +0900

    Update ircbot.py

--- ircbot.py
+++ ircbot.py
@@ -49,8 +49,8 @@ if __name__ == '__main__':
 			weather = soup.find(id="dfs-panel")
 			currenttemp = weather.find(class_="now_weather1_right temp1 MB10").get_text()
 			currenttemp = currenttemp.strip('\n\r')
-			tempf = weather.select("po_seoul.temp") #trying to get this to work
-			#css_soup.select("dl.dd.temp")
+			tempf = weather.select("dl.dd.temp") #trying to get this to work
+			#css_soup.select("po_seoul.temp")
 			if len(name) < 22: #username limit
 				ircmsg == ircmsg.lower()
 				if message.find('hi ' + botnick) != -1:
