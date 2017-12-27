c8b850234ae47a3e79816a43847702ca8bb5c760
MotivatorNZ <34211218+MotivatorNZ@users.noreply.github.com>
2017-12-27 20:31:45 +0900

    Update ircbot.py

--- ircbot.py
+++ ircbot.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
 		if msgcodet == "PRIVMSG": 
 			name = ircmsg.split('!',1)[0][1:] #splitting out the name from msgcodet
 			message = ircmsg.split('PRIVMSG',1)[1].split(':',1)[1]
-			weather = soup.select("po_seoul.temp")
+			weather = soup.select("po_seoul.temp").get_text()
 			#soup.find_all(class_="temp")
 			#currenttemp = weather.select("po_seoul.temp")
 			#findall("dd", class_="temp") #trying to get this to work
