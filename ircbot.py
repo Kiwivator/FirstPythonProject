d1a2d15ae5f29314c79abb8b021c1d6c60c965e5
MotivatorNZ <34211218+MotivatorNZ@users.noreply.github.com>
2017-12-29 15:35:09 +0900

    Update ircbot.py

--- ircbot.py
+++ ircbot.py
@@ -53,6 +53,8 @@ if __name__ == '__main__':
 		if msgcodet == "PRIVMSG": 
 			name = ircmsg.split('!',1)[0][1:] #splitting out the name from msgcodet
 			message = ircmsg.split('PRIVMSG',1)[1].split(':',1)[1]
+			source = ircmsg.split('PRIVMSG',1)[0].split(':',1)[0]
+			print source
 			
 			if len(name) < 22: #username limit
 				ircmsg == ircmsg.lower()
