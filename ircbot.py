8702607391a536adddda1bc999cae276ef89b608
MotivatorNZ <34211218+MotivatorNZ@users.noreply.github.com>
2017-12-29 12:10:36 +0900

    Update ircbot.py

--- ircbot.py
+++ ircbot.py
@@ -31,6 +31,7 @@ def gettemp(currenttemp):
 	currenttemp = seoul.find(class_="temp").get_text()
 	sendmsg('The current temperature in Seoul is ' + currenttemp + '°c.')
 	print (currenttemp)
+	del currenttemp
 
 if __name__ == '__main__':
 	ircsock.connect((server, 6667))
