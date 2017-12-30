2393d4fc7d8c083e1422bf540003416740910708
MotivatorNZ <34211218+MotivatorNZ@users.noreply.github.com>
2017-12-30 13:16:11 +0900

    Update ircbot.py

--- ircbot.py
+++ ircbot.py
@@ -29,6 +29,7 @@ def gettemp():
 	weather = soup.find(id="weather")
 	seoul = weather.find(class_="po_seoul")
 	currenttemp = seoul.find(class_="temp").get_text()
+	currenttemp = float(currenttemp)
 	currenttempf = (currenttemp * 1.8) + 32
 	sendmsg('The current temperature in Seoul is ' + currenttemp + '°c (' + currenttempf + ')')
 	print (currenttemp)
