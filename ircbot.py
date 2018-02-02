b3e18c17e022f9372b4bf64949f66147b8f69f1c
MotivatorNZ <34211218+MotivatorNZ@users.noreply.github.com>
2018-02-02 17:54:09 +0900

    Update ircbot.py

--- ircbot.py
+++ ircbot.py
@@ -1,53 +1,26 @@
-#!/usr/binn/python3
+#!/usr/bin/python3
 import socket
-import requests
-from bs4 import BeautifulSoup
+
+
 
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server = "irc.snoonet.org"
-channel = "#Korean" 
-botnick = "Botivator"
-adminname = "MotivatorAFK"
-exitcode = "bye " + botnick
-host = "user/Motivator"
-
+server = "irc.snoonet.org" # Server
+channel = "##mashlytest" # Channel
+botnick = "Bashly" # Your bots nick
+adminname = "mashly" # Your IRC nickname
+exitcode = "Bye" + botnick
+host = "user/mashly"
 def joinchan(chan):
 	ircsock.send(bytes("JOIN "+ chan +"\n", "UTF-8"))
-	#Below is the original join code.
 	#ircmsg = ""
-	#while ircmsg.find("End of message of the day.") == -1:
-		#ircmsg = ircsock.recv(2048).decode("UTF-8")
-		#ircmsg = ircmsg.strip('\n\r')
-		#print(ircmsg)
-
-def sendmsg(msg, target=channel):
+	#while ircmsg.find("End of /NAMES list.") == -1
+	#	ircmsg = ircsock.recv(2048).decode("UTF-8")
+	#	ircmsg = ircmsg.strip('\n\r')
+	#	print (ircmsg)
+def sendmsg(msg, target=channel): #sends message to the target
 	ircsock.send(bytes("PRIVMSG "+ target +" :"+ msg +"\n", "UTF-8"))
 	
-def gettemp(city):
-	city = city.lower()
-	index = "po_"
-	cityi = index + city
-	print (cityi)
-	page = requests.get("http://www.weather.go.kr/weather/main-now-weather.jsp")
-	soup = BeautifulSoup(page.content, 'html.parser')
-	weather = soup.find(id="weather")
-	citytemp = weather.find(class_=cityi)
-	try:
-		currenttemp = citytemp.find(class_="temp").get_text()
-		currenttemp = float(currenttemp)
-		currenttempf = (currenttemp * 1.8) + 32
-		currenttempf = float(currenttempf)
-		city = city.capitalize()
-		sendmsg('The current temperature in ' + city + ' is ' + str(currenttemp) + '°C (' + str(currenttempf) + '°F).')
-		print (str(currenttemp) + "/" + str(currenttempf))
-	
-	except:
-		message = "Sorry, weather for this city isn't available, but may be added later."
-		sendmsg(message, source)
-		
-
-
 if __name__ == '__main__':
 	ircsock.connect((server, 6667))
 	ircsock.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick + " " + botnick + "\n", "UTF-8"))
@@ -58,72 +31,62 @@ if __name__ == '__main__':
 		ircmsg = ircmsg.strip('\n\r')
 		print(ircmsg)
 		
-		msgcode = ircmsg.split()[0] #splitting the first part of RAW irc message
+		msgcode = ircmsg.split()[0]
 		msgcodet = ircmsg.split()[1]
-
+		
 		if msgcode == "001": #code that snoonet sends out when ready for join command
 			joinchan(channel)
-			
-		joinchan(channel) #needs a second join command to connect to channel successfully
+
+		joinchan(channel)
 		
-		if msgcodet == "PRIVMSG": 
-			name = ircmsg.split('!',1)[0][1:] #splitting out the name from msgcodet
-			namehost = ircmsg.split('@',1)[1].split(' ',1)[0]
-			message = ircmsg.split('PRIVMSG',1)[1].split(':',1)[1]
+		if msgcodet == "PRIVMSG":
+			name = ircmsg.split('!',1)[0][1:] #splitting out name from msgcodet
+			namehost = ircmsg.split('@',1)[1].split(' ',1)[1]
+			message = ircmsg.split('PRIVMSG' ,1)[1].split(':',1)[1]
 			source = ircmsg.split('PRIVMSG ',1)[1].split(':',1)[0]
 			print (source)
 			print (namehost)
 			
-			if len(name) < 22: #username limit
+			if len(name) < 17: #username limit
 				ircmsg = ircmsg.lower()
 				botnick = botnick.lower()
 				message = message.lower()
-				if message.find('hi ' + botnick) != -1:
+				if message.find('Hi ' + botnick) != -1:
 					print (message)
-					sendmsg("Hello " + name + "!")
-				
+					sendmsg ("Hello " + name + "!")
 				if source == botnick:
 					sendmsg(message, adminname)
-				
+						
 				if host == namehost and message[:5].find('.tell') != -1:
 					print(message)
-					print (len(message))
-					if len(message) == 5: #TODO: this can probably be refactored with other error message
+					print  (len(message))
+					if len(message) == 5:
 						message = "Please enter the name of a target and message. "
 						target = name
-							
+					
 					else:
 						try:
-							target = message.split(' ', 1)[1]
+							target = message.split(' ',1)[1]
 						except IndexError:
-							message = "Please enter the name of a target and message. "
+							message = "Could not parse. The message should be in the format of '.tell [target] [message]' to work properly."
 							target = name
 							
 						if target.find(' ') != -1:
-							message = target.split(' ', 1)[1]
+							message = target.split(' ',1)[1]
 							target = target.split(' ')[0]
 						else:
 							target = name
-							message = "Please try again. Message should be in the format of '.tell [target] [message]' to work properly."
-				
-					sendmsg(message, target)
-					
-				if message[:5].find('.temp') != -1:
-					try:
-						city = message.split(' ', 1)[1]
-						print (city)
-						gettemp(city)
-						
-					except IndexError:
-						message = "Please enter .temp and the name of a city."
-						target = name
-						sendmsg(message, source)			
+							message = "Could not parse. The message should be in the format of '.tell [target] [message]' to work properly."
+					sendmsg(message, source)
 
-					
-				if host == namehost and message.rstrip() == exitcode:
-					sendmsg("As you wish. :'(")
+				if host == namehost and message.rstrip()== exitcode:
+					sendmsg("kthxbye ")
 					ircsock.send(bytes("QUIT \n", "UTF-8"))
-					ircsock.close() #broken?
-		elif msgcode == "PING":
-			ircsock.send(bytes("PONG " + ircmsg.split()[1] + "\r\n", "UTF-8")) #sending back a pong including custom ping code
-			print("Sent PONG " + ircmsg.split()[1])
+					ircsock.close()
+			
+			else:
+				if msgcode == "PING":
+					ircsock.send(bytes("PONG " + ircmsg.split()[1] + "\r\n", "UTF-8"))
+					
+				
+				
