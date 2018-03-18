16de997d7512b4b83e494b372767062b302db803
MotivatorNZ <34211218+MotivatorNZ@users.noreply.github.com>
2018-03-19 00:31:35 +0900

    Update ircbot.py

--- ircbot.py
+++ ircbot.py
@@ -35,7 +35,7 @@ def roulette():
 		print ("Dead by random number")
 		ircsock.send(bytes("KICK " + name + diemsg + "\n", "UTF-8"))
 		count = 6
-	#elif i == 1:
+	elif i == 1:
 		#sendmsg('CLICK. You survived. There are 5 chances left.')
 	elif count == 1:
 		print ("Dead by last bullet")
