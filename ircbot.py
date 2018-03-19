735d3b8e792afe9c8d5745721034fdafef9251f9
MotivatorNZ <34211218+MotivatorNZ@users.noreply.github.com>
2018-03-19 23:26:19 +0900

    Update ircbot.py

--- ircbot.py
+++ ircbot.py
@@ -40,7 +40,7 @@ def roulette():
 		ircsock.send(bytes("KICK " + " " + channel + " " + name + " " + diemsg + "\n", "UTF-8"))
 		count = 6
 	else:
-		sendmsg('CLICK. You survived. There are ' + count + 'chances left.')
+		sendmsg('CLICK. You survived. There are ' + str(count) + 'chances left.')
 		count = count - 1
 	
 def gettemp(city):
