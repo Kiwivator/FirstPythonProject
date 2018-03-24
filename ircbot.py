be3346b41eb8496b1e115cd08f7a4c4d545723a9
MotivatorNZ <34211218+MotivatorNZ@users.noreply.github.com>
2018-03-24 22:17:29 +0900

    Update ircbot.py

--- ircbot.py
+++ ircbot.py
@@ -85,11 +85,10 @@ def yaja():
 	message = "야자타임 will now begin for 15 minutes. Everyone is free to use 반말 to each other until 야자타임 ends. Have fun and be nice!~"
 	sendmsg(message, source)
 	mins = 15
-	mins = mins - 5
 	while mins > 5:
+		mins = mins - 5
 		time.sleep(300)
 		sendmsg("야자 타임 " + str(mins) + "분 남았습니다.")
-		mins = mins - 5
 	if mins == 5:
 		sendmsg("야자 타임 " + str(mins) + "분 남았습니다. Prepare your 요s.")
 		mins = mins - 1
