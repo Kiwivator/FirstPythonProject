564231576fe189531cb798abdc8dd8ccfb989eec
MotivatorNZ <34211218+MotivatorNZ@users.noreply.github.com>
2018-03-31 19:20:49 +0900

    Update ircbot.py

--- ircbot.py
+++ ircbot.py
@@ -12,8 +12,8 @@ from bs4 import BeautifulSoup
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server = "irc.snoonet.org"
-channel = "##motitest" 
-botnick = "Botivatortest"
+channel = "#Korean" 
+botnick = "Botivator"
 adminname = "MotivatorAFK"
 exitcode = "bye " + botnick
 host = "user/Motivator"
