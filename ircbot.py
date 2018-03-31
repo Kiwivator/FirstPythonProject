97bedcc57a010703e3ec6d5f2baa0a4de3d5eb65
MotivatorNZ <34211218+MotivatorNZ@users.noreply.github.com>
2018-03-31 19:16:56 +0900

    Update ircbot.py

--- ircbot.py
+++ ircbot.py
@@ -196,12 +196,9 @@ if __name__ == '__main__':
 						roulette(name)
 						
 				if message.find('hotpot') != -1:
-					now = datetime.datetime.now()
-					print (now.year)
 					print (todaydate)
-					#print (str(todaydate)
-					#if todaydate < datetime.today():
-					#	todaypot = 0
+					if todaydate < datetime.date.today():
+						todaypot = 0
 					hotpot += 1
 					if hotpot == 5:
 						todaypot += 5
