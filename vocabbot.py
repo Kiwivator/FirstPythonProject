85f757335421f93c0c937286175ad83673dbbf2d
MotivatorNZ <34211218+MotivatorNZ@users.noreply.github.com>
2018-04-02 22:25:17 +0900

    Update vocabbot.py

--- vocabbot.py
+++ vocabbot.py
@@ -12,8 +12,8 @@ botnick = "VocabBot"
 adminname = "MotivatorAFK"
 exitcode = "bye " + botnick
 host = "user/Motivator"
-global lastmessage
-lastmessage = "No previous messages."
+#global lastmessage
+#lastmessage = "No previous messages."
 
 def joinchan(chan):
 	ircsock.send(bytes("JOIN "+ chan +"\n", "UTF-8"))
@@ -56,15 +56,15 @@ if __name__ == '__main__':
 		joinchan(channel) #needs a second join command to connect to channel successfully
 		
 		if msgcodet == "PRIVMSG": 
-			print (lastmessage)
+			#print (lastmessage)
 			name = ircmsg.split('!',1)[0][1:] #splitting out the name from msgcodet
 			namehost = ircmsg.split('@',1)[1].split(' ',1)[0]
 			message = ircmsg.split('PRIVMSG',1)[1].split(':',1)[1]
-			message = lastmessage
+			#message = lastmessage
 			source = ircmsg.split('PRIVMSG ',1)[1].split(':',1)[0]
 			print (source)
 			print (namehost)
-			print ("The last message was: " + lastmessage)
+			#print ("The last message was: " + lastmessage)
 			
 			if len(name) < 22: #username limit
 				ircmsg = ircmsg.lower()
